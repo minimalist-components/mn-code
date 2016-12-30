12a44066efda8ea62706f3f2f3501fd70db8c824
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-30 11:10:00 -0200

    using temp dependency from forked repo#branch

--- tasks/vendorCSS.js
+++ tasks/vendorCSS.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp'
 import bowerFiles from 'bower-files'
-import minifyCss from 'gulp-minify-css'
+// import minifyCss from 'gulp-minify-css'
+// import minifyCss from 'gulp-clean-css'
 import concat from 'gulp-concat'
 
 gulp.task('vendorCSS', vendorCSSTask)
@@ -20,6 +21,6 @@ function vendorCSSTask() {
   return gulp
     .src(dependencies)
     .pipe(concat('vendor.css'))
-    // .pipe(minifyCss({keepSpecialComments: 0}))
+    // .pipe(minifyCss())
     .pipe(gulp.dest('./docs'))
 }
